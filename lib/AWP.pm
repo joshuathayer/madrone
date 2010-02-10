74ec485f3f29fa84f9dbbd63c143c63cdb7e8866
Joshua Thayer <joshua@Joshua-Thayers-MacBook-Pro.local>
2010-02-10 00:04:20 -0800

    adds function for parsing an entire directory at a time

--- lib/AWP.pm
+++ lib/AWP.pm
@@ -1,5 +1,8 @@
 package AWP;
 
+use strict;
+use Data::Dumper;
+
 =head1 NAME
 
 AWP - An XHTML-based templating system.
@@ -11,6 +14,33 @@ Version 0.01
 =cut
 
 our $VERSION = '0.01';
+
+sub parseDirectory {
+	my $templateDir = shift;
+	my $modDir = shift;
+	my $namednodes = shift;
+
+	my $ret = {};
+
+	opendir(TDIR, "$templateDir/");
+	my @templates = readdir(TDIR);
+	closedir(TDIR);
+
+	foreach my $template (@templates) {
+
+		my ($tname) = $template =~ /(.*)\.xhtml/;		
+		next unless $tname;
+
+    	my $p = AWP::Parser->new();
+    	$p->setNamedNodes($namednodes);
+    	$p->includeMods($modDir);
+    	$p->parsefile("$templateDir/$template");
+    	$ret->{$tname} = $p;
+	}
+
+	return $ret;
+
+}
 
 
 =head1 SYNOPSIS
